6baec32438ad293456a05c93c4a5b9b41f50cedc
Elijah Conley <elijahandaleciael@gmail.com>
2020-01-26 19:21:19 -0500

    add route and imports to files in app js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Header from "./components/Header.js";
 
+import CharacterList from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage";
+import SearchForm from "./components/SearchForm";
+import { Route } from "react-router-dom";
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path ="/" component={WelcomePage}/>
+      <Route path="/Search" component={SearchForm}/>
+      <Route path="/Characters" component={CharacterList}/>
     </main>
   );
 }
