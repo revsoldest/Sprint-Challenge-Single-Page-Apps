e7025f696531f754b76ef0f15ef98bba77f396fd
Elijah Conley <elijahandaleciael@gmail.com>
2020-01-26 20:22:55 -0500

    done with list and cards need more style

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,39 @@
 import React from "react";
+import styled from "styled-components"
+  function CharacterCard(props) {
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+    const Card = styled.div `
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    text-align: center;
+    background-color: Lightblue;
+    margin: 5% 30% 5% 30%;
+    border: solid 5px black;
+    `
+    const Head = styled.h2 `
+    font-size: 300%;
+    `
+    const HeaderImg = styled.img`
+  width: 100px;
+  height: 100px;
+  border: 1px solid blue;
+  `
+
+  // image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+
+
+
+  return (
+    
+    <Card>
+      <Head> {props.name}</Head> 
+      <HeaderImg src={props.image} />
+        <p>Species: {props.species}</p>
+        <p>Status: {props.status}</p>
+        <p>Location: {props.location}</p>
+    </Card>
+  )
 }
+export default CharacterCard;
+
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,80 @@
 import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import CharacterCard from "./CharacterCard";
+import {Link} from "react-router-dom";
 
-export default function CharacterList() {
+function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [character, setCharacter] = useState([]);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
+    Axios
+      .get('https://rickandmortyapi.com/api/character/')
+      .then(response => {
+
+        const charSearch = response.data.results.filter(search =>
+          search.name.toLowerCase().includes(query.toLowerCase())
+        );
+        console.log(response.data.results);
+        setCharacter(charSearch);
+        // setCharacter(response.data.results)
+        // console.log(response.data.results)
+      })
+      .catch(error =>{
+        console.error('Server error', error)
+      })
+
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+  }, [query]);
+  const handleInputChange = event =>{
+    setQuery(event.target.value);
+  };
 
   return (
     <section className="character-list">
       <h2>TODO: `array.map()` over your state here!</h2>
+      <div>
+      <form>
+        <input
+        type = "text"
+        onChange = {handleInputChange}
+        value = {query}
+        name = "name"
+        tabIndex = "0"
+        placeholder = "Find Character"
+        autoComplete = "off"
+        />
+      </form>
+    </div>
+
+    <section className="character-list">
+      
+      <Link to= {"/"}>
+      <button>Home</button>
+      </Link>
+      <div >
+        {character.map(char => {
+
+          return(
+            <CharacterCard 
+            key = {char.id} 
+            image = {char.image}
+            name = {char.name} 
+            species={char.species} 
+            status={char.status}
+            location={char.location.name} /> 
+          )
+        })}    
+      </div>
+
+        <Link to= {"/Home"}>
+      <button>Home</button>
+      </Link>
+
+    </section>
     </section>
   );
 }
+export default CharacterList;
